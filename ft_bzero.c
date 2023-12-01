ce331ece46e1fa3d485ac4388d97e3757b788084
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2023-12-01 23:13:45 +0100

    Create bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/29 20:08:06 by damateos          #+#    #+#             */
-/*   Updated: 2023/12/01 23:02:37 by damateos         ###   ########.fr       */
+/*   Created: 2023/12/01 22:49:55 by damateos          #+#    #+#             */
+/*   Updated: 2023/12/01 23:12:22 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
-
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-void	*ft_memset(void *b, int c, size_t len);
-size_t	ft_strlen(const char *s);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-void	ft_bzero(void *s, size_t n);
-
-#endif
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
