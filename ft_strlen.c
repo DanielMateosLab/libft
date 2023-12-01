09fede22ac7679b751c0d62a811ade8ed17377a1
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2023-12-01 23:13:35 +0100

    Refactor includes

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,11 +6,11 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 17:23:50 by damateos          #+#    #+#             */
-/*   Updated: 2023/09/16 17:26:22 by damateos         ###   ########.fr       */
+/*   Updated: 2023/12/01 22:56:47 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 size_t	ft_strlen(const char *s)
 {
