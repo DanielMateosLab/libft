49cbeb2b029047d9179861c7d2b69e023ca775cc
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2023-12-05 23:11:27 +0100

    Implement memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,20 +6,19 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 20:10:55 by damateos          #+#    #+#             */
-/*   Updated: 2023/12/02 20:30:30 by damateos         ###   ########.fr       */
+/*   Updated: 2023/12/05 23:11:10 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// We have to avoid this
-// aabb___ copy 4 chars from 1 to 2:
-// 1 2  ^: end of inserted text
-// aaaaaa_ Unintented: result should be aaaabb
-// Copying backwards we solve this
-// if src is before A, copy backwards.
-// otherwise, copy forwards.
-void	*memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	
+	if (!dst && !src)
+		return (dst);
+	if (dst < src)
+		return (ft_memcpy(dst, src, len));
+	while (len--)
+		((char *)dst)[len] = ((char *)src)[len];
+	return (dst);
 }
