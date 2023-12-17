77c10cf9ee56696d99dc7f07d70242c006f0652a
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2023-12-17 18:40:06 +0100

    Update strlcpy implementation

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,23 +6,27 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 12:24:05 by damateos          #+#    #+#             */
-/*   Updated: 2023/12/07 15:04:57 by damateos         ###   ########.fr       */
+/*   Updated: 2023/12/17 18:39:50 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// Si el destize es mayor o igual que src len + 1, copiamos src 
+// entera (con su null terminator) en dst.
+// Si no, copiamos dstsize - 1 bytes de src en dst y le ponemos
+// un null terminator al final
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
+	size_t	src_len;
 
-	i = 0;
-	while (i + 1 < dstsize && src[i])
+	src_len = ft_strlen(src);
+	if (dstsize >= src_len + 1)
+		ft_memcpy(dst, src, src_len + 1);
+	else if (dstsize != 0)
 	{
-		dst[i] = src[i];
-		i++;
+		ft_memcpy(dst, src, dstsize - 1);
+		dst[dstsize - 1] = '\0';
 	}
-	if (dstsize > 0)
-		dst[i] = '\0';
-	return (i);
+	return (src_len);
 }
