f2cfe33f0a76da601f5a0bcf446dfca294fd0c80
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-16 22:33:47 +0100

    Implement memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,13 +6,22 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 23:09:01 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/15 23:11:01 by damateos         ###   ########.fr       */
+/*   Updated: 2024/01/16 22:27:46 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int _c, size_t n)
 {
+	unsigned char	*str;
+	unsigned char	c;
 
+	str = (unsigned char *)s;
+	c = (unsigned char)_c;
+	while (*str != c && n--)
+		str++;
+	if (n && *str == c)
+		return ((void *)str);
+	return (0);
 }
