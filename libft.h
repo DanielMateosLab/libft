e387b1570dc9854b0d7d224942286e5649024ed6
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-16 22:45:39 +0100

    Implement memcmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 20:08:06 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/15 23:08:45 by damateos         ###   ########.fr       */
+/*   Updated: 2024/01/16 22:36:03 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
