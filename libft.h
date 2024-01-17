f0426926d5f168525b7e074bac59a6a77527b6c7
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-17 22:32:31 +0100

    Implement atoi

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 20:08:06 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/16 22:59:26 by damateos         ###   ########.fr       */
+/*   Updated: 2024/01/17 22:26:11 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_atoi(const char *str);
 
 #endif
