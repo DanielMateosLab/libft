fcc0c4070fa6b125e1ffa60c434b793fbd562731
damateos <damateos@student.42malaga.com>
2024-01-18 21:59:28 +0100

    Fix strlcat implementation

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 18:12:00 by damateos          #+#    #+#             */
-/*   Updated: 2023/12/17 19:35:04 by damateos         ###   ########.fr       */
+/*   Updated: 2024/01/18 21:57:12 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,21 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	src_len;
 	size_t	dst_len;
+	size_t	i;
 
 	src_len = ft_strlen(src);
-	dst_len = ft_strlen(dst);
+	dst_len = 0;
+	i = 0;
+	while (dst[dst_len] && dst_len < dstsize)
+		dst_len++;
 	if (dst_len >= dstsize)
 		return (dstsize + src_len);
-	if (src_len < dstsize - dst_len)
-		ft_memcpy(dst + dst_len, src, src_len + 1);
-	else
+	while (src[i] && dst_len + i < dstsize - 1)
 	{
-		ft_memcpy(dst + dst_len, src, dstsize - 1);
-		dst[dst_len + dstsize - 1] = '\0';
+		dst[dst_len + i] = src[i];
+		i++;
 	}
+	if (dst_len + i < dstsize)
+		dst[dst_len + i] = '\0';
 	return (src_len + dst_len);
 }
