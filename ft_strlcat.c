439bb359f3a393c2d413e74803ed2b80107ca590
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-20 18:50:25 +0100

    Refactor strlcat to use memcpy

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 18:12:00 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/18 21:57:12 by damateos         ###   ########.fr       */
+/*   Updated: 2024/01/20 18:48:55 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,19 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	src_len;
 	size_t	dst_len;
-	size_t	i;
 
 	src_len = ft_strlen(src);
 	dst_len = 0;
-	i = 0;
 	while (dst[dst_len] && dst_len < dstsize)
 		dst_len++;
 	if (dst_len >= dstsize)
 		return (dstsize + src_len);
-	while (src[i] && dst_len + i < dstsize - 1)
+	if (dstsize > dst_len + src_len)
+		ft_memcpy(dst + dst_len, src, src_len + 1);
+	else
 	{
-		dst[dst_len + i] = src[i];
-		i++;
+		ft_memcpy(dst + dst_len, src, dstsize - dst_len - 1);
+		dst[dstsize - 1] = '\0';
 	}
-	if (dst_len + i < dstsize)
-		dst[dst_len + i] = '\0';
 	return (src_len + dst_len);
 }
