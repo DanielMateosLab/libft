d329061cc17b2ae526c162fdb08e2374243a5fb1
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-20 17:58:21 +0100

    Use strlcpy in substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,33 +6,35 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 22:25:26 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/18 23:23:04 by damateos         ###   ########.fr       */
+/*   Updated: 2024/01/20 17:56:58 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t dest_len)
 {
 	char	*sub;
-	char	*str_ptr;
-	size_t	s_len;
+	size_t	source_len;
 
 	if (s == NULL)
 		return (NULL);
-	s_len = ft_strlen(s);
-	if (start > s_len)
-		len = 0;
-	else if (len > s_len - start)
-		len = s_len - start;
-	sub = malloc((len + 1) * sizeof(char));
+	source_len = ft_strlen(s);
+	if (start > source_len)
+		dest_len = 0;
+	else if (dest_len > source_len - start)
+		dest_len = source_len - start;
+	sub = malloc((dest_len + 1) * sizeof(char));
 	if (sub == NULL)
 		return (NULL);
-	s += start;
-	str_ptr = sub;
-	*(sub + len) = '\0';
-	while (len-- && *s)
-		*sub++ = *s++;
-	return (str_ptr);
+	if (start > source_len)
+		sub[0] = '\0';
+	else
+		ft_strlcpy(sub, s + start, dest_len + 1);
+	return (sub);
 }
-// (len > s_len - start) ensures that we don't overflow s
+
+// edge cases (think that the length of a str is == to the last index):
+//	s == NULL
+// 	start > source_len
+// 	dest_len > source_len - start (max to copy): we limit dest_len
