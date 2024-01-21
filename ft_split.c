af2cdd69aa666d0e738259d00073795111ba43fa
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-21 12:42:41 +0100

    Refactor split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 09:44:51 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/21 11:56:18 by damateos         ###   ########.fr       */
+/*   Updated: 2024/01/21 12:42:08 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,52 +33,67 @@ static size_t	count_words(char const *s, char c)
 	return (count);
 }
 
-static char	**clean_arr(char **arr, size_t len)
+static char	*clean_arr(char **arr, size_t len)
 {
-	while (len--)
+	size_t	i;
+
+	i = 0;
+	while (i < len)
 	{
-		free(arr[len]);
-		arr[len] = NULL;
+		free(arr[i]);
+		arr[i] = NULL;
+		i++;
 	}
 	free(arr);
 	arr = NULL;
 	return (NULL);
 }
 
-// 'hola que  tal '
+static char	*make_word(char **arr, const char *start, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (arr[i])
+		i++;
+	arr[i] = ft_substr(start, 0, len);
+	if (!arr[i])
+		return (clean_arr(arr, i));
+	return (arr[i]);
+}
+
+static size_t	get_word_len(const char *word_start, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (word_start[len] != c && word_start[len])
+		len++;
+	return (len);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	size_t		words_count;
+	size_t		words;
 	const char	*word_start;
-	size_t		word_len;
 	char		**arr;
-	size_t		arr_i;
 
 	if (!s)
 		return (NULL);
-	words_count = count_words(s, c);
 	word_start = NULL;
-	word_len = 0;
-	arr_i = 0;
-	arr = (char **)malloc((words_count + 1) * sizeof(char *));
+	arr = (char **)ft_calloc(((words = count_words(s, c)) + 1), sizeof(char *));
 	if (!arr)
 		return (NULL);
-	arr[words_count] = NULL;
+	arr[words] = NULL;
 	while (*s)
 	{
 		if (!word_start && *s != c)
 			word_start = s;
-		if (word_start && *s != c)
-			word_len++;
 		if (word_start && (*s == c || !*(s + 1)))
 		{
-			if (*s != c)
-				word_len++;
-			arr[arr_i++] = ft_substr(word_start, 0, word_len);
-			if (!arr[arr_i - 1])
-				return (clean_arr(arr, arr_i));
+			if (!make_word(arr, word_start, get_word_len(word_start, c)))
+				return (NULL);
 			word_start = NULL;
-			word_len = 0;
 		}
 		s++;
 	}
