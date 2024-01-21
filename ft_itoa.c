e531eb7eff965240fe7fd20c933fe685ee850736
damateos <damateos@student.42malaga.com>
2024-01-21 18:34:54 +0100

    Improve itoa readability

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,13 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 17:23:06 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/21 18:24:17 by damateos         ###   ########.fr       */
+/*   Updated: 2024/01/21 18:33:54 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	get_size(int n)
+size_t static	get_size(int n)
 {
 	size_t	size;
 
@@ -27,25 +27,37 @@ size_t	get_size(int n)
 	return (size);
 }
 
+void static	handle_min(char *res, int *n, size_t *i)
+{
+	if (*n == -2147483648)
+	{
+		res[*i] = '8';
+		(*i)--;
+		(*n) /= 10;
+	}
+}
+
+void static	handle_negative(char *res, int *n)
+{
+	if (*n < 0)
+	{
+		res[0] = '-';
+		(*n) *= -1;
+	}
+}
+
 char	*ft_itoa(int n)
 {
 	size_t	size;
 	char	*res;
 
-	res = malloc(sizeof(char) * ((size = get_size(n)) + 1));
+	size = get_size(n);
+	res = malloc(sizeof(char) * (size + 1));
 	if (!res)
 		return (NULL);
 	res[size--] = '\0';
-	if (n == -2147483648)
-	{
-		res[size--] = '8';
-		n /= 10;
-	}
-	if (n < 0)
-	{
-		res[0] = '-';
-		n *= -1;
-	}
+	handle_min(res, &n, &size);
+	handle_negative(res, &n);
 	if (n == 0)
 		res[0] = '0';
 	while (n != 0)
