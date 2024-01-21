888c18106c0171e85188eea719a1955bf5e6b599
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-21 11:52:57 +0100

    Add split implementation

---
+++ ft_split.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/21 09:44:51 by damateos          #+#    #+#             */
+/*   Updated: 2024/01/21 11:52:17 by damateos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	count_words(char const *s, char c)
+{
+	size_t	count;
+	int		in_word;
+
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (!in_word && *s != c)
+		{
+			in_word = 1;
+			count++;
+		}
+		if (in_word && *s == c)
+			in_word = 0;
+		s++;
+	}
+	return (count);
+}
+
+static char	**clean_arr(char **arr, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		free(arr[i]);
+		arr[i] = NULL;
+		i++;
+	}
+	free(arr);
+	arr = NULL;
+	return (NULL);
+}
+
+// 'hola que  tal '
+char	**ft_split(char const *s, char c)
+{
+	size_t		words_count;
+	const char	*word_start;
+	size_t		word_len;
+	char		**arr;
+	size_t		arr_i;
+
+	if (!s)
+		return (NULL);
+	words_count = count_words(s, c);
+	word_start = NULL;
+	word_len = 0;
+	arr_i = 0;
+	arr = (char **)malloc((words_count + 1) * sizeof(char *));
+	if (!arr)
+		return (NULL);
+	arr[words_count] = NULL;
+	while (*s)
+	{
+		if (!word_start && *s != c)
+			word_start = s;
+		if (word_start && *s != c)
+			word_len++;
+		if (word_start && (*s == c || !*(s + 1)))
+		{
+			if (*s != c)
+				word_len++;
+			arr[arr_i++] = ft_substr(word_start, 0, word_len);
+			if (!arr[arr_i - 1])
+				return (clean_arr(arr, arr_i));
+			word_start = NULL;
+			word_len = 0;
+		}
+		s++;
+	}
+	return (arr);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 20:08:06 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/20 19:39:25 by damateos         ###   ########.fr       */
+/*   Updated: 2024/01/21 10:11:05 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
