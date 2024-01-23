ba9e72f5f936aecb7ae0fe55570a2f009493fa5d
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-23 22:56:10 +0100

    Implement lstadd_back

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: default <default@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 20:08:06 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/23 22:37:37 by default          ###   ########.fr       */
+/*   Updated: 2024/01/23 22:41:52 by default          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
