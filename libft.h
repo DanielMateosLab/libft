1760049587a6471c8ae498ca3fdf1a92437ced48
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-23 22:36:52 +0100

    implement lstsize

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: default <default@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 20:08:06 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/23 22:15:56 by default          ###   ########.fr       */
+/*   Updated: 2024/01/23 22:22:43 by default          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ typedef struct s_list
 
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
 
 #endif
