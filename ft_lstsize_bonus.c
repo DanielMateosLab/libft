2022d06be90905ad67f725ac43336e5a72f7d1a4
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-23 22:41:17 +0100

    Minor refactor

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -6,7 +6,7 @@
 /*   By: default <default@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 22:23:00 by default           #+#    #+#             */
-/*   Updated: 2024/01/23 22:36:26 by default          ###   ########.fr       */
+/*   Updated: 2024/01/23 22:40:01 by default          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_lstsize(t_list *lst)
 {
 	int	size;
 
-	if (lst == NULL)
+	if (!lst)
 		return (0);
 	size = 1;
 	while (lst->next)
