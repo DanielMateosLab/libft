616db22ab9879ee05bea1e8652f4e8a02e5f3708
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-01-24 19:17:54 +0100

    Implement lstiter

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: default <default@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 20:08:06 by damateos          #+#    #+#             */
-/*   Updated: 2024/01/23 23:05:11 by default          ###   ########.fr       */
+/*   Updated: 2024/01/24 19:11:51 by default          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
