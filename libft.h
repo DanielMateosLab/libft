353991a1cbad341212c1a8ce1d5eac7cfa7fb22e
damateos <damateos@student.42.fr>
2024-06-29 16:49:59 +0200

    Add fr_free utility

--- libft.h
+++ libft.h
@@ -53,6 +53,7 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
+void			*ft_free(void **ptr);
 
 typedef struct s_list
 {
