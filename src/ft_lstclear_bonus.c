c94dfc6f0c31b00bebde8baad1d2112e2ba21e31
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-10-31 21:37:55 +0100

    FIx issue in lstclear

--- src/ft_lstclear_bonus.c
+++ src/ft_lstclear_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: default <default@student.42.fr>            +#+  +:+       +#+        */
+/*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 23:05:27 by default           #+#    #+#             */
-/*   Updated: 2024/01/23 23:16:39 by default          ###   ########.fr       */
+/*   Updated: 2024/10/31 21:37:38 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	node = *lst;
 	if (node->next)
 		ft_lstclear(&(node->next), del);
-	del(node->content);
+	if (del)
+		del(node->content);
 	free(node);
 	*lst = NULL;
 }
