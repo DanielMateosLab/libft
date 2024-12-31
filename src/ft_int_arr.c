8e5802de46be5ee9a9056e6e805c07d772a20d05
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-12-31 12:51:08 +0100

    fix: add missing ft_swap

--- src/ft_int_arr.c
+++ src/ft_int_arr.c
@@ -6,15 +6,15 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 18:15:12 by damateos          #+#    #+#             */
-/*   Updated: 2024/12/30 18:30:40 by damateos         ###   ########.fr       */
+/*   Updated: 2024/12/31 12:49:07 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int *ft_int_arr_copy(int *arr, size_t len)
+int	*ft_int_arr_copy(int *arr, size_t len)
 {
-	int	*copy;
+	int		*copy;
 	size_t	i;
 
 	copy = (int *)ft_calloc(len, sizeof(int));
@@ -40,6 +40,15 @@ void	ft_int_arr_print(int *arr, size_t len)
 		i++;
 	}
 	ft_printf("\n");
+}
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void	ft_int_arr_reverse(int *arr, size_t len)
